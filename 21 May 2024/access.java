e233854ea7d3982bdb07a1e1bb3b9d1f11adbc7e
lisyjayakumar <122440078+lisyjayakumar@users.noreply.github.com>
2024-05-21 17:12:55 +0530

    Add files via upload

---
+++ 21 May 2024/access.java
@@ -0,0 +1,11 @@
+public class access {
+    final int x = 10;
+    final double PI = 3.14;
+  
+    public static void main(String[] args) {
+        access myObj = new access();
+      System.out.println(myObj.x); 
+      System.out.println(myObj.PI); 
+    }
+  }
+  
