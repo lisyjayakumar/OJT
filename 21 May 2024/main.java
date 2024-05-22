0ec2ec33751a0a29118122179a079e140de8d6a2
lisyjayakumar <122440078+lisyjayakumar@users.noreply.github.com>
2024-05-22 17:18:35 +0530

    Delete 21 May 2024/main.java

--- 21 May 2024/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-      person myObj = new person();
-      myObj.setName("John"); // Set the value of the name variable to "John"
-      System.out.println(myObj.getName());
-    }
-  }
