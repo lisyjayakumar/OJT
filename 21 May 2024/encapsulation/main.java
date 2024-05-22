f063f4b9ee4f6f8a4ec5f30604905a6515fae40f
lisyjayakumar <122440078+lisyjayakumar@users.noreply.github.com>
2024-05-22 17:17:50 +0530

    Add files via upload

---
+++ 21 May 2024/encapsulation/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+      person myObj = new person();
+      myObj.setName("John"); // Set the value of the name variable to "John"
+      System.out.println(myObj.getName());
+    }
+  }
