3f019ad6ea988e82bb040d5cf121d126938856ad
lisyjayakumar <122440078+lisyjayakumar@users.noreply.github.com>
2024-05-29 16:59:39 +0530

    Add files via upload

---
+++ 29 May 2024/breakandcontinue.java
@@ -0,0 +1,14 @@
+public class breakandcontinue {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<=10){
+            
+            if(i%5==0){
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
